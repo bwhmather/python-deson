4da37ca5e3ba6a65ca70f27871ac6ac6cf546bfc
Ben Mather <bwhmather@bwhmather.com>
2017-07-08 15:13:36 +0100

    Make value argument optional.  If not passed, parsers return a closure

--- deson/__init__.py
+++ deson/__init__.py
@@ -3,74 +3,103 @@ import numbers
 import math
 
 
-def parse_datetime(value, *, required=True):
-    raise NotImplementedError()
+_unset = object()
 
 
-def parse_int(value, *, max_value=None, min_value=None, required=True):
-    if value is None:
-        if required:
-            raise TypeError()
-        else:
-            return None
-
-    if not isinstance(value, numbers.Real):
-        raise TypeError()
-
-    if abs(value) >= 2 ** sys.float_info.mant_dig:
-        raise ValueError()
-
-    fractional, integer = math.modf(value)
-    if fractional != 0:
-        raise ValueError()
-
-    integer = int(integer)
-
-    if max_value is not None and integer > max_value:
-        raise ValueError()
-
-    if min_value is not None and integer < min_value:
-        raise ValueError()
-
-    return integer
-
-
-def parse_float(value, *, max_value=None, min_value=None, required=True):
-    if value is None:
-        if required:
-            raise TypeError()
-        else:
-            return None
-
-    if not isinstance(value, numbers.Real):
-        raise TypeError()
-
-    if max_value is not None and value > max_value:
-        raise ValueError()
-
-    if min_value is not None and value < min_value:
-        raise ValueError()
-
-    return float(value)
-
-
-def parse_string(
-    value, *, max_len=None, min_len=None, pattern=None, required=True
+def parse_datetime(
+    value=_unset, *, required=True
 ):
     raise NotImplementedError()
 
 
-def parse_bytes(value, *, max_len=None, min_len=None, required=True):
+def parse_int(
+    value=_unset, *, max_value=None, min_value=None, required=True
+):
+    def parse(value):
+        if value is None:
+            if required:
+                raise TypeError()
+            else:
+                return None
+
+        if not isinstance(value, numbers.Real):
+            raise TypeError()
+
+        if abs(value) >= 2 ** sys.float_info.mant_dig:
+            raise ValueError()
+
+        fractional, integer = math.modf(value)
+        if fractional != 0:
+            raise ValueError()
+
+        integer = int(integer)
+
+        if max_value is not None and integer > max_value:
+            raise ValueError()
+
+        if min_value is not None and integer < min_value:
+            raise ValueError()
+
+        return integer
+
+    if value is not _unset:
+        return parse(value)
+    else:
+        return parse
+
+
+def parse_float(
+    value=_unset, *, max_value=None, min_value=None, required=True
+):
+    def parse(value):
+        if value is None:
+            if required:
+                raise TypeError()
+            else:
+                return None
+
+        if not isinstance(value, numbers.Real):
+            raise TypeError()
+
+        if max_value is not None and value > max_value:
+            raise ValueError()
+
+        if min_value is not None and value < min_value:
+            raise ValueError()
+
+        return float(value)
+
+    if value is not _unset:
+        return parse(value)
+    else:
+        return parse
+
+
+def parse_string(
+    value=_unset, *, max_len=None, min_len=None, pattern=None, required=True
+):
     raise NotImplementedError()
 
 
-def parse_set(value, *, max_len=None, min_len=None, required=True):
+def parse_bytes(
+    value=_unset, *, max_len=None, min_len=None, required=True
+):
     raise NotImplementedError()
 
 
-def parse_array(value, *, max_len=None, min_len=None, required=True):
+def parse_set(
+    value=_unset, *, max_len=None, min_len=None, required=True
+):
     raise NotImplementedError()
 
 
-def parse_dictionary(value, *, schema, allow_extra=True, required=True):
+def parse_array(
+    value=_unset, *, max_len=None, min_len=None, required=True
+):
+    raise NotImplementedError()
+
+
+def parse_dictionary(
+    value=_unset, *, schema, allow_extra=True, required=True
+):
     raise NotImplementedError()
